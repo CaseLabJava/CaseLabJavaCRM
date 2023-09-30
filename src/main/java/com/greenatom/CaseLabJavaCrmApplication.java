bd02a76b859c561a1b3da6ed9f10c70f7880c186
79969 <m.byckoff2015@yandex.ru>
2023-09-30 21:02:33 +0300

    init: initial commit

---
+++ src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
@@ -0,0 +1,13 @@
+package com.greenatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseLabJavaCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseLabJavaCrmApplication.class, args);
+    }
+
+}

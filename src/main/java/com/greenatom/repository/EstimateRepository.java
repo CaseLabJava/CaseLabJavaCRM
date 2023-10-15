8331ab06b02e8fcfdf66c61a877054f07f32f9c5
Andrey Nachevny <nochergt@gmail.com>
2023-10-15 17:24:12 +0300

    add annotation

--- src/main/java/com/greenatom/repository/EstimateRepository.java
+++ src/main/java/com/greenatom/repository/EstimateRepository.java
@@ -2,6 +2,8 @@ package com.greenatom.repository;
 
 import com.greenatom.domain.entity.Estimate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstimateRepository extends JpaRepository<Estimate, Long> {
 }

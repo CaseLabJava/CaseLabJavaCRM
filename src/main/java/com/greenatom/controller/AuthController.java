218a879517afbad4ad5c11633cdc4fc0edf2d303
79969 <m.byckoff2015@yandex.ru>
2023-10-18 16:11:08 +0300

    feat: add @override annotations

--- src/main/java/com/greenatom/controller/AuthController.java
+++ src/main/java/com/greenatom/controller/AuthController.java
@@ -17,21 +17,25 @@ public class AuthController implements AuthApi {
     private final AuthService authService;
 
     @PostMapping("/signUp")
+    @Override
     public JwtResponse registration(@RequestBody EmployeeDTO employeeDTO){
         return authService.registration(employeeDTO);
     }
 
     @PostMapping("/signIn")
+    @Override
     public JwtResponse login(@RequestBody AuthDto authDto){
         return authService.login(authDto);
     }
 
     @PostMapping("/accessToken")
+    @Override
     public JwtResponse getAccessToken(@RequestBody RefreshJwtRequest refreshJwtRequest){
         return authService.getAccessToken(refreshJwtRequest.getRefreshToken());
     }
 
     @PostMapping("/refresh")
+    @Override
     public JwtResponse refresh(@RequestBody RefreshJwtRequest refreshJwtRequest){
         return authService.refresh(refreshJwtRequest.getRefreshToken());
     }

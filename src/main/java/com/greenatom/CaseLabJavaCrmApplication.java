723c7c58a13f7215089bcf88786afe40cad3b327
PUPILUPI <bev6889@gmail.com>
2023-10-22 16:57:10 +0300

    test commit for pull request

--- src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
+++ src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
@@ -2,8 +2,10 @@ package com.greenatom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value ="")
 public class CaseLabJavaCrmApplication {
 
     public static void main(String[] args) {

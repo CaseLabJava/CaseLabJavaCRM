055217c2c1a54925c767944aaf85d3dd3541a603
VazhnovPavel <pavel.fortex@yandex.ru>
2023-10-25 23:12:43 +0530

    Feat: Начало работы с загрузкой файла

---
+++ src/main/java/com/greenatom/controller/FileUploadController.java
@@ -0,0 +1,36 @@
+package com.greenatom.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileUploadController {
+
+    @PostMapping(value ="/upload",produces = {"application/json"})
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                String uploadDir = "/Users/fortex/Documents/www/download";
+                String fileName = file.getOriginalFilename();
+                File uploadPath = new File(uploadDir);
+                if (!uploadPath.exists()) {
+                    uploadPath.mkdirs();
+                }
+
+                File targetFile = new File(uploadPath, fileName);
+                try (FileOutputStream fos = new FileOutputStream(targetFile)) {
+                    fos.write(file.getBytes());
+                }
+                return "Файл успешно загружен.";
+            } catch (IOException e) {
+                return "Ошибка при загрузке файла: " + e.getMessage();
+            }
+        } else {
+            return "Файл пустой, загрузка не выполнена.";
+        }
+    }
+}

e483d7490aceab339296b89465dbcddacec6c5dd
VazhnovPavel <pavel.fortex@yandex.ru>
2023-10-27 10:37:05 +0530

    Fix: none

--- src/main/java/com/greenatom/controller/FileUploadController.java
+++ src/main/java/com/greenatom/controller/FileUploadController.java
@@ -17,27 +17,4 @@ public class FileUploadController {
     }
 
 }
-//    @PostMapping(value ="/upload",consumes = "multipart/form-data")
-//    public String uploadFile(@RequestParam("file") MultipartFile file) {
-//        if (!file.isEmpty()) {
-//            try {
-//                String uploadDir = "/Documents/UploadDoc";
-//                String fileName = file.getOriginalFilename();
-//                File uploadPath = new File(uploadDir);
-//                if (!uploadPath.exists()) {
-//                    uploadPath.mkdirs();
-//                }
-//
-//                File targetFile = new File(uploadPath, fileName);
-//                try (FileOutputStream fos = new FileOutputStream(targetFile)) {
-//                    fos.write(file.getBytes());
-//                }
-//                return "Файл успешно загружен.";
-//            } catch (IOException e) {
-//                return "Ошибка при загрузке файла: " + e.getMessage();
-//            }
-//        } else {
-//            return "Файл пустой, загрузка не выполнена.";
-//        }
-//    }
-//}
+
--- src/main/java/com/greenatom/domain/dto/UploadDocDTO.java
+++ src/main/java/com/greenatom/domain/dto/UploadDocDTO.java
@@ -1,5 +1,6 @@
 package com.greenatom.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Класс для запроса загрузки документа")
 public class UploadDocDTO {
 
+    @Schema(description = "Id заказа")
     private Long id;
 
+    @Schema(description = "Полный путь к документу")
     private String linkToFolder;
 
     private String fileName;
-
-    private String filePath;
 
 }

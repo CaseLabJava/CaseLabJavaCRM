8bb3416b50c3a9556bc8b0359cf645558a3c558f
79969 <m.byckoff2015@yandex.ru>
2023-10-29 22:24:06 +0300

    fix: delete PasswordsNotMatchingException

--- src/main/java/com/greenatom/service/exceptions/PasswordsNotMatchingException.java
+++
@@ -1,9 +0,0 @@
-package com.greenatom.service.exceptions;
-
-public class PasswordsNotMatchingException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public PasswordsNotMatchingException() {
-        super("Passwords doesn't match!");
-    }
-}

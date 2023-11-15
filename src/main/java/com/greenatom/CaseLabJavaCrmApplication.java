3d10776bb2239a66d3b9db0e66e0ee37caf7c897
PUPILUPI <bev6889@gmail.com>
2023-11-15 21:55:12 +0300

    commit to start docker

--- src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
+++ src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class CaseLabJavaCrmApplication {
 
     public static void main(String[] args) {

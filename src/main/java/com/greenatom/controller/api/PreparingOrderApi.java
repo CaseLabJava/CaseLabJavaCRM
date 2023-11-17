c21f1e89b565740216bd47952e76efe32f96dcc0
Andrey Nachevny <nochergt@gmail.com>
2023-11-18 01:09:06 +0300

    feat: add sorting and pagination for preparing order by Criteria API

--- src/main/java/com/greenatom/controller/api/PreparingOrderApi.java
+++ src/main/java/com/greenatom/controller/api/PreparingOrderApi.java
@@ -10,9 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
+import java.time.Instant;
 
 @Tag(name = "PreparingOrder API", description = "API для работы со сборкой заказа")
 public interface PreparingOrderApi {
@@ -26,18 +28,44 @@ public interface PreparingOrderApi {
                                     schema = @Schema(implementation = PreparingOrderResponseDTO.class)
                             )
                     }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Сборки заказа по переданным параметрам не существует",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = EmployeeErrorMessage.class)
+                            )
+                    }
             )
     })
     @Operation(
             summary = "Получение всех заказов по статусу"
     )
-    ResponseEntity<List<PreparingOrderResponseDTO>> getPreparingOrders(@Parameter(description = "Начальная страница") Integer pageNumber,
-                                                                       @Parameter(description = "Кол-во страниц") Integer pageSize,
-                                                                       @Parameter(
+    ResponseEntity<Page<PreparingOrderResponseDTO>> getPreparingOrders(@Parameter(description = "Начальный номер страницы", example = "0") Integer pageNumber,
+                                                                       @Parameter(description = "Размер страницы", example = "10") Integer pageSize,
+                                                                       @Parameter(description = "Id заказа", example = "1") Long orderId,
+                                                                       @Parameter(description = "Id кладовщика", example = "1") Long employeeId,
+                                                                       @Parameter(description = "Статус сборки заказа",
                                                                                in = ParameterIn.QUERY,
                                                                                name = "status",
-                                                                               schema = @Schema(allowableValues = {"DONE", "WAITING_FOR_PREPARING"}))
-                                                                       String status);
+                                                                               schema = @Schema(allowableValues = {
+                                                                                       "WAITING_FOR_PREPARING",
+                                                                                       "IN_PROCESS", "DONE"})) String deliveryStatus,
+                                                                       @Parameter(description = "Начало сборки заказа",
+                                                                               example ="2016-01-06T15:22:53.403Z" ) Instant startTime,
+                                                                       @Parameter(description = "Конец сборки",
+                                                                               example = "2016-01-06T15:23:53.403Z") Instant endTime,
+                                                                       @Parameter(description = "Поле для сортировки") String sortBy,
+                                                                       @Parameter(
+                                                                               in = ParameterIn.QUERY,
+                                                                               description = "Порядок сортировки",
+                                                                               name = "sortDirection",
+                                                                               schema = @Schema(allowableValues = {
+                                                                                       "ASC",
+                                                                                       "DESC"
+                                                                               })) Sort.Direction sortDirection);
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",

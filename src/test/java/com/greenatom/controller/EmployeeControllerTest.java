d9d2e1d7dbb159193ff00b0470fa158d1ad15acc
VazhnovPavel <pavel.fortex@yandex.ru>
2023-11-18 21:44:37 +0530

    feat: add CI pipeline

--- src/test/java/com/greenatom/controller/EmployeeControllerTest.java
+++ src/test/java/com/greenatom/controller/EmployeeControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @SpringBootTest
 @ActiveProfiles("test")

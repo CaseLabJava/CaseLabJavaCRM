e112350f050825e5f47ed64bf9b19727161145b6
79969 <m.byckoff2015@yandex.ru>
2023-11-26 23:22:14 +0300

    feat: add claim support for self-service

---
+++ Client-Self-Service/src/main/java/com/greenatom/clientselfservice/contoller/ClaimController.java
@@ -0,0 +1,46 @@
+package com.greenatom.clientselfservice.contoller;
+
+import com.greenatom.clientselfservice.domain.dto.claim.ClaimRequestDTO;
+import com.greenatom.clientselfservice.domain.dto.claim.ClaimResponseDTO;
+import com.greenatom.clientselfservice.domain.mapper.ClaimMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("self_service/claim")
+@RequiredArgsConstructor
+public class ClaimController {
+    private final RestTemplate restTemplate;
+
+    private final ClaimMapper claimMapper;
+
+    @PostMapping
+        public ResponseEntity<ClaimResponseDTO> addClaim(@RequestBody ClaimRequestDTO claim) {
+        Objects.requireNonNull(restTemplate.postForObject(getUrl(), claim,
+                ClaimRequestDTO.class));
+        return ResponseEntity.ok(claimMapper.toResponse(claim));
+    }
+    @GetMapping
+    public List<ClaimResponseDTO> findByUser(
+            @RequestParam(defaultValue = "0") Integer pageNumber,
+            @RequestParam(defaultValue = "10") Integer pageSize
+    ) {
+        Map<String, Integer> uriParam = new HashMap<>();
+        uriParam.put("pageSize", pageSize);
+        uriParam.put("pageNumber", pageNumber);
+        return Arrays.stream(Objects.requireNonNull(
+                restTemplate.getForObject(getUrl()
+                             //   +"?pageNumber="+pageNumber+"&pageSize" +pageSize
+                        ,
+                        ClaimResponseDTO[].class,uriParam))).toList();
+    }
+
+    private String getUrl() {
+        return "http://CaseLabJavaCrm/api/claims";
+    }
+
+}

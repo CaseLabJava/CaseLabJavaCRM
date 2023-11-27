1b0a0c11dea83c18d8fb3181e79319b3eb3aeec0
Andrey Nachevny <nochergt@gmail.com>
2023-11-27 14:36:56 +0300

    hotfix: disable cors

--- src/main/java/com/greenatom/controller/AuthController.java
+++ src/main/java/com/greenatom/controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/auth")
-
+@CrossOrigin
 public class AuthController implements AuthApi {
 
     private final AuthService authService;

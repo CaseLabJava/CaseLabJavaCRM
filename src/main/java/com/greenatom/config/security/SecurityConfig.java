40ba59c0c596c63b9356be6592598bf7a9653beb
Andrey Nachevny <nochergt@gmail.com>
2023-11-27 13:41:44 +0300

    hotfix: disable cors

--- src/main/java/com/greenatom/config/security/SecurityConfig.java
+++ src/main/java/com/greenatom/config/security/SecurityConfig.java
@@ -86,19 +86,4 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // Specify the allowed origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600); // Max age of the CORS pre-flight request
-            }
-        };
-    }
-
 }

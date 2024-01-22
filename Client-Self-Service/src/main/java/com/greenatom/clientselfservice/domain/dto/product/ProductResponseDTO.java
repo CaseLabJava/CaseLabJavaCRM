2f05f98f871f4faf2414a38a62c15daf3c1dd563
Andrey Nachevny <nochergt@gmail.com>
2024-01-22 23:20:53 +0300

    hotfix: make class serializable

--- Client-Self-Service/src/main/java/com/greenatom/clientselfservice/domain/dto/product/ProductResponseDTO.java
+++ Client-Self-Service/src/main/java/com/greenatom/clientselfservice/domain/dto/product/ProductResponseDTO.java
@@ -3,14 +3,14 @@ package com.greenatom.clientselfservice.domain.dto.product;
 import com.greenatom.clientselfservice.domain.entity.OrderItem;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class ProductResponseDTO {
+public class ProductResponseDTO implements Serializable {
     private Long id;
 
     private String productName;
